5618e319748bed2a6e314608c930c448ad640d9c
neda_ <neda@nda.localdomain>
2020-09-17 21:16:44 +0430

    Updates

---
+++ precache-manifest.e4a4bd0326cd597134921edf17e18542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d546318b9a18e16c87f397265c3408",
+    "url": "/reactjs-card-challenge/index.html"
+  },
+  {
+    "revision": "bf3b1e03dda686dee706",
+    "url": "/reactjs-card-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "786e81a2099d1361378c",
+    "url": "/reactjs-card-challenge/static/js/2.3bee33e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-card-challenge/static/js/2.3bee33e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3b1e03dda686dee706",
+    "url": "/reactjs-card-challenge/static/js/main.24f3787a.chunk.js"
+  },
+  {
+    "revision": "3ac1955675e4ff9a32cb",
+    "url": "/reactjs-card-challenge/static/js/runtime-main.cfd2d842.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-card-challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
